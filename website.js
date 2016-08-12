a15becbb4bc35e5dd2440df5f5fb925ebf7fd1cd
Felicityrb <felicitygirl05@gmail.com>
2016-08-12 15:44:31 -0800

    My first website!

---
+++ website.js
@@ -0,0 +1,13 @@
+function changing(){
+    var img = document.getElementById("hi");
+    img.src="hussie.jpg";
+}
+function photo(){
+    var img = document.getElementById("wassup");
+    img.src="ball.jpg";
+}
+
+function Loading(){
+    alert("There is more then meets the eye about me! I prefer they/them pronouns.");
+}
+
